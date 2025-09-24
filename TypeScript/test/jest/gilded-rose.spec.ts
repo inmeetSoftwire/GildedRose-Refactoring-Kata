c90766401b6ab1133cf042979c3b6002b635fe7d
Inmeet Khurana <Inmeet.Khurana@softwire.com>
2025-09-24 13:56:12 +0100

    Added unit tests for regular and expired items

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -6,4 +6,25 @@ describe('Gilded Rose', () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
+
+  it('decreases in quality and sellIn for standard item', () => {
+    const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", 10, 20)]);
+    const items = gildedRose.updateQuality()
+    expect(items).toEqual([{
+      name: "+5 Dexterity Vest",
+      sellIn: 9,
+      quality: 19
+    }])
+  })
+
+  it('decreases twice in quality if past sellIn date', () => {
+    const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", -2, 20)]);
+    const items = gildedRose.updateQuality()
+    expect(items).toEqual([{
+      name: "+5 Dexterity Vest",
+      sellIn: -3,
+      quality: 18
+    }])
+  })
+
 });
