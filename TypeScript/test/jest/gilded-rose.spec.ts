42453de59eafbb193bdf3474894c0feaf75277f7
Inmeet Khurana <Inmeet.Khurana@softwire.com>
2025-09-25 12:13:31 +0100

    Split up testing into one describe block per item type and one behaviour per it block

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,13 +1,7 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
-  });
-
-  it('decreases in quality and sellIn for standard item', () => {
+describe("Standard items", () => {
+  it('should decrease in quality and sellIn', () => {
     const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", 10, 20)]);
     const items = gildedRose.updateQuality()
     expect(items).toEqual([{
@@ -17,7 +11,7 @@ describe('Gilded Rose', () => {
     }])
   })
 
-  it('decreases twice in quality if past sellIn date', () => {
+  it('should decrease twice in quality if past sellIn date', () => {
     const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", -2, 20)]);
     const items = gildedRose.updateQuality()
     expect(items).toEqual([{
@@ -26,7 +20,103 @@ describe('Gilded Rose', () => {
       quality: 18
     }])
   })
+})
 
+describe('Aged Brie', () => {
+    it('should increase in quality by 1 when sellIn is positive', () => {
+    const gildedRose = new GildedRose(
+      [
+        new Item("Aged Brie", 10, 0),
+      ]);
+    const items = gildedRose.updateQuality()
+    expect(items).toEqual([
+      {
+        name: "Aged Brie",
+        sellIn: 9,
+        quality: 1
+      }
+    ])
+  })
+
+  it('should increase in quality by 2 when sellin is <= 0', () => {
+    const gildedRose = new GildedRose(
+      [
+        new Item("Aged Brie", -8, 0)
+      ]);
+    const items = gildedRose.updateQuality()
+    expect(items).toEqual([
+      {
+        name: "Aged Brie",
+        sellIn: -9,
+        quality: 2
+      }
+    ])
+  })
+})
+
+describe('Backstages Passes', () => {
+  it('increase in quality by 1 when sellIn is more than 10', () => {
+    const gildedRose = new GildedRose(
+      [
+        new Item("Backstage passes to Softwire Christmas Party", 12, 40),
+      ]);
+    const items = gildedRose.updateQuality()
+    expect(items).toEqual([
+      {
+        name: "Backstage passes to Softwire Christmas Party",
+        sellIn: 11,
+        quality: 41
+      }
+    ])
+  })
+
+
+  it('increase in quality by 2 when sellIn is between 6 and 10', () => {
+    const gildedRose = new GildedRose(
+      [        new Item("Backstage passes to Ryan's 27th Birthday Bash", 9, 40),
+      ]);
+    const items = gildedRose.updateQuality()
+    expect(items).toEqual([
+      {
+        name: "Backstage passes to Ryan's 27th Birthday Bash",
+        sellIn: 8,
+        quality: 42
+      }
+    ])
+  })
+
+  it('increase in quality by 3 when sellIn is between 0 and 5', () => {
+    const gildedRose = new GildedRose(
+      [
+        new Item("Backstage passes to Cambridge University", 3, 40),
+      ]);
+    const items = gildedRose.updateQuality()
+    expect(items).toEqual([
+      {
+        name: "Backstage passes to Cambridge University",
+        sellIn: 2,
+        quality: 43
+      },
+    ])
+  })
+
+  it('quality is 0 when sellIn is negative', () => {
+    const gildedRose = new GildedRose(
+      [
+         new Item("Backstage passes to Company Meeting", 0, 40)
+      ]);
+    const items = gildedRose.updateQuality()
+    expect(items).toEqual([
+      {
+        name: "Backstage passes to Company Meeting",
+        sellIn: -1,
+        quality: 0
+      },
+    ])
+  })
+})
+
+describe("Quality limits", () => {
   it('does not allow items quality to become negative', () => {
     const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", -5, 0)]);
     const items = gildedRose.updateQuality()
@@ -37,33 +127,10 @@ describe('Gilded Rose', () => {
     }])
   })
 
-  it('aged brie increases in quality the older it gets', () => {
-    const gildedRose = new GildedRose(
-      [
-        new Item("Aged Brie", 10, 0),
-        new Item("Aged Brie", -8, 0)
-      ]);
-    const items = gildedRose.updateQuality()
-    expect(items).toEqual([
-      {
-        name: "Aged Brie",
-        sellIn: 9,
-        quality: 1
-      }, 
-      {
-        name: "Aged Brie",
-        sellIn: -9,
-        quality: 2
-      }
-    ])
-  })
-
   it('does not allow quality of items to go above 50', () => {
     const gildedRose = new GildedRose(
       [
         new Item("Aged Brie", 5, 50),
-        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-        new Item("Elixir of the Mongoose", 5, 49)
       ]);
     const items = gildedRose.updateQuality()
     expect(items).toEqual([
@@ -71,52 +138,16 @@ describe('Gilded Rose', () => {
         name: "Aged Brie",
         sellIn: 4,
         quality: 50
-      }, 
-      {
-        name: "Backstage passes to a TAFKAL80ETC concert",
-        sellIn: 4,
-        quality: 50
-      },
-      {
-        name: "Elixir of the Mongoose",
-        sellIn: 4,
-        quality: 48
       },
     ])
   })
+})
 
-  it('does not allow quality of items to go above 50', () => {
-    const gildedRose = new GildedRose(
-      [
-        new Item("Aged Brie", 5, 50),
-        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-        new Item("Elixir of the Mongoose", 5, 49)
-      ]);
-    const items = gildedRose.updateQuality()
-    expect(items).toEqual([
-      {
-        name: "Aged Brie",
-        sellIn: 4,
-        quality: 50
-      }, 
-      {
-        name: "Backstage passes to a TAFKAL80ETC concert",
-        sellIn: 4,
-        quality: 50
-      },
-      {
-        name: "Elixir of the Mongoose",
-        sellIn: 4,
-        quality: 48
-      },
-    ])
-  })
-
+describe("Sulfuras items", () => {
   it('"Sulfuras" items are unchanged', () => {
     const gildedRose = new GildedRose(
       [
-        new Item("Sulfuras", 0, 80),
-         new Item("Sulfuras, Hand of Ragnaros", -1, 80)
+        new Item("Sulfuras", 0, 80)
       ]);
     const items = gildedRose.updateQuality()
     expect(items).toEqual([
@@ -124,46 +155,7 @@ describe('Gilded Rose', () => {
         name: "Sulfuras",
         sellIn: 0,
         quality: 80
-      }, 
-      {
-        name: "Sulfuras, Hand of Ragnaros",
-        sellIn: -1,
-        quality: 80
       },
     ])
   })
-
-  it('"Backstage passes" are unchanged', () => {
-    const gildedRose = new GildedRose(
-      [
-        new Item("Backstage passes to Softwire Christmas Party", 12, 40),
-        new Item("Backstage passes to Ryan's 27th Birthday Bash", 9, 40),
-        new Item("Backstage passes to Cambridge University", 3, 40),
-         new Item("Backstage passes to Company Meeting", 0, 40)
-      ]);
-    const items = gildedRose.updateQuality()
-    expect(items).toEqual([
-      {
-        name: "Backstage passes to Softwire Christmas Party",
-        sellIn: 11,
-        quality: 41
-      }, 
-      {
-        name: "Backstage passes to Ryan's 27th Birthday Bash",
-        sellIn: 8,
-        quality: 42
-      },
-      {
-        name: "Backstage passes to Cambridge University",
-        sellIn: 2,
-        quality: 43
-      },
-      {
-        name: "Backstage passes to Company Meeting",
-        sellIn: -1,
-        quality: 0
-      },
-    ])
-  })
-
-});
+})
